750adfa2aea9a6163891a32573ff524206cc8f7e
unknown <joelmenacho@gmail.com>
2022-10-14 19:15:16 -0500

    update Data.js

---
+++ components/Data/data.js
@@ -0,0 +1,13 @@
+const data = [
+    {
+        id: 1,
+        title: "Laptop Gamer",
+        description: "Laptop Gamer Asus TUF Dash F15 FX517ZC 15.6'' i5-12450H 512GB SSD 8G RTX3050 4GB Black",
+        image_url: "https://oechsle.vteximg.com.br/arquivos/ids/10884247-1000-1000/2156312.jpg?v=637970467132970000",
+        price: 22,
+        rating: 5,
+        route: "laptop-gamer"
+    }
+];
+
+export default data;
